d3313029d7858ca37e03692cf66b4ee1e94a5b84
dpritchett <dpritchett@gmail.com>
2010-09-29 14:17:03 -0500

    minor animation added

--- spells.js
+++ spells.js
@@ -1,20 +1,28 @@
-function takeTurn() {
-	if($("input").attr("value") != "") {
+function takeTurn(inVal) {
+	if(inVal != "") {
 		$(".gameout li:first").remove();
 		$(".gameout").append(
 		   "<li>" +
-		   $("input").attr("value") +
+		   inVal +
 		   "</li>");
 		 $("input").removeAttr("value");
+		 $(".gameout li:last").effect("bounce", {direction: "down", distance: "7", times: "2"})
 		} 
-		
+	$("input").select();
 };
 
 $(document).ready(function() {
 	$("#inputs").append(
-		"<input value=\"What next?\" size=55/><button>Do it!</button>");
+		"<input value=\"What do you want to do?\" tabindex=1 size=55/><button type=submit tabindex=2>Do it!</button>");
 	$("button").click(
-		function () {takeTurn();}); 
+		function () {
+			takeTurn($("input").attr("value"));
+		}); 
 	$("input").click( function() { $(this).select(); });
 	$("input").select();
+	$("input").keypress(function(e) {
+		if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+			$("button").click();
+		}
+	});
 });
