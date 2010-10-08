e4bcb96fbda6d4afcbcaff23ad2846e4966e0448
Daniel J. Pritchett <dpritchett@gmail.com>
2010-10-08 14:37:44 -0500

    midway through a coffee conversion

--- public/chatbox.js
+++ public/chatbox.js
@@ -1,128 +1,97 @@
-// toggler.value() returns true then false then true then false...
-var toggler = (function() {
-        var flag = false;
-        return {
-                value: function() {
-                               flag = !flag;
-                               return flag;
-                       }
-        };
-})();
-
-var packet = {
-        name: "default_name",
-        content: "",
-        date: ""
-};
-
-//Display an alert in a colored box that fades in and then out
-function alertUser(alertText){
-        $("#alerts").empty().append(alertText).stop(true, true).fadeIn(
-                        500,
-                        function(){$("#alerts").fadeOut(3000);});
-}
-
-//Write a line to the chatbox on the page
-function spitLine(contents, username) {
-        //no username means no timestamp etc
-        if(username) {
-                if(username == 'chatbot') {
-                        contents = "<strong>" + contents + "</strong>";
-                }
-                var d = new Date();
-                contents = "<span class=\"timestamp\">[" +
-                        ((d.getHours() % 12) || '12') + //12-hour time
-                        ":" +
-                        (d.getMinutes() + 100).toString().substring(1) + //force 2-digit minutes
-                        "]</span> " +
-                        "<span style=\"background-color: #" +
-                        $.md5(username).substring(0,6) + //unique color for each username
-                        "\">" +
-                        username + "<\/span>: " + contents;
-        }
-        //handle empty line
-        if(!contents) {contents = '&nbsp;';}
-
-        //push new line to bottom of list while losing oldest line
-        $(".gameout").append(
-                        "<li>" +
-                        contents +
-                        "<\/li>");
-        $(".gameout li:first").remove();
-
-        //zebra stripes
-        if(toggler.value()) {$(".gameout li:last").addClass("alt");}
-}
-
-//clears the chatbox by pushing empty lines
-function wipeScreen(printMe){
-        for(i = 0; i < 10; i++) {spitLine();}
-        spitLine(printMe);
-}
-
-//Reads user input and passes it on to server via websocket.
-//Also checks for slash commands
-function takeTurn(inVal) {
-        //clean up whitespace
-        //need to sanitize inputs serverside too
-        inVal = inVal.replace(" ","&nbsp;").replace(/\\/gi, "").replace(/\"/gi, "");
-
-        packet.name =  $("#username").attr('value');
-        packet.content =  inVal;
-        packet.date = (new Date()).getTime();
-
-        //Detect and execute slash commands
-        if(inVal.charAt(0) == '/')
-        {
-                if(inVal=="\/clear"){
-                        wipeScreen();
-                }
-                //switch username both on page and in packet
-                //alert server of the new name
-                if((inVal.search("\/name") != -1) || (inVal.search("\/nick") != -1)){
-                        packet.name = inVal.substr(11);
-                        var msg = {
-                                name: packet.name,
-                                system: "/nick"
-                        };
-                        $("#username").attr("value", msg.name);
-                        socket.send(msg);
-                }
-                alertUser(inVal.substr(1)); //user needs to see slash commands are received
-        }
-
-        //Submit plaintext to server as JSON
-        else if(inVal !="") {
-                socket.send(packet);
-                spitLine(packet.content, packet.name);
-        } 
-
-        //Clear and target input blank
-        $("#txtYourMove").removeAttr("value").select();
-
-        //hide username input once user has replaced default name
-        if($("#username").attr("value").search("user") == -1) {
-                $("#username").css('display', 'none');
-        }
-}
-
-// onload init stuff
-$(document).ready(function() {
+(function() {
+  var alertUser, packet, spitLine, takeTurn, toggler, wipeScreen;
+  toggler = function() {
+    var flag, value;
+    flag = false;
+    return (value = function() {
+      return (flag = !flag);
+    });
+  };
+  packet = {
+    name: 'default_name',
+    content: '',
+    date: ''
+  };
+  alertUser = function(alertText) {
+    return $('#alerts').empty().append(alertText).stop(true, true).fadeIn(500, function() {
+      return $('#alerts').fadeOut(3000);
+    });
+  };
+  spitLine = function(contents, username) {
+    var _ref, d;
+    if (typeof username !== "undefined" && username !== null) {
+      if (username === 'chatbot') {
+        contents = "<strong>" + contents + "</strong>";
+      }
+      d = new Date();
+      contents = '<span class=\"timestamp\">[' + ("" + ((d.getHours() % 12) || '12')) + ':' + (d.getMinutes() + 100).toString().substring(1) + ']</span><span style=\"background-color: #' + $.md5(username).substring(0, 6) + ("\">" + (username) + " <\/span>: " + (contents));
+    }
+    if (!(typeof contents !== "undefined" && contents !== null)) {
+      contents = '&nbsp;';
+    }
+    $(".gameout").append("<li>" + (contents) + "<\/li>");
+    $(".gameout li:first").remove();
+    return (typeof (_ref = toggler.value()) !== "undefined" && _ref !== null) ? $(".gameout li:last").addClass("alt") : null;
+  };
+  wipeScreen = function(printMe) {
+    var i;
+    i = 0;
+    while (i < 10) {
+      spitLine();
+      i++;
+    }
+    return spitLine(printMe);
+  };
+  takeTurn = function(inVal) {
+    var msg;
+    inVal = inVal.replace(' ', '&nbsp;').replace(/\\/gi, '').replace(/\"/gi, '');
+    packet.name = $('#username').attr('value');
+    packet.content = inVal;
+    packet.date = (new Date()).getTime();
+    if (inVal.charAt(0) === '/') {
+      if (inVal === '\/clear') {
         wipeScreen();
-        $("#username").attr("value", "user" + Math.floor(Math.random()*1000));
-        $("button").click(
-                function () {
-                        takeTurn($("#txtYourMove").attr("value"));
-                }); 
-        $("#txtYourMove").click( function() { $(this).select(); }).click();
-        //Submit on <ENTER>
-        $("#txtYourMove").keypress(function(e) {
-                if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-                        $("button").click();
-                }
-        });
-        packet.name = $("#username").attr("value");
-        socket.send({
-                name: packet.name,
-                system: "~onjoin"});
-});
+      }
+      if (inVal.search('\/name') !== -1 || inVal.search('\/nick') !== -1) {
+        packet.name = inVal.substr(11);
+        msg = {
+          name: packet.name,
+          system: '/nick'
+        };
+        $('#username').attr("value", msg.name);
+        return socket.send(msg);
+      }
+      /*
+      alertUser inVal.substr(1) #user needs to see slash commands are received
+              #Submit plaintext to server as JSON
+      else if inVal != ''
+              socket.send packet
+      spitLine packet.content, packet.name
+
+              #Clear and target input blank
+              $("#txtYourMove").removeAttr("value").select()
+
+              #hide username input once user has replaced default name
+              if $("#username").attr("value").search("user") == -1
+      $("#username").css 'display', 'none'
+
+      # onload init stuff
+      $(document).ready () ->
+              wipeScreen()
+              $("#username").attr "value",
+      "user #{Math.floor(Math.random() * 1000)}"
+              $("button").click () ->
+      takeTurn($("#txtYourMove").attr("value"))
+              $("#txtYourMove").click( () ->
+      $(this).select()
+              ).click()
+
+              #Submit on <ENTER>
+              $("#txtYourMove").keypress (e) ->
+      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13))
+              $("button").click()
+              packet.name = $("#username").attr("value")
+              socket.send { name: packet.name, system: "onjoin"}*/
+    }
+  };
+}).call(this);
--- server.coffee
+++ server.coffee
@@ -9,22 +9,23 @@
 #    http://github.com/dpritchett
 #    http://twitter.com/dpritchett
 
-$PORT = 80
-$DB_SERVER = 'dpritchett.couchone.com'
-$DB_PORT = 80
+PORT        = 80
+DB_SERVER   = 'dpritchett.couchone.com'
+DB_PORT     = 80
 
-Connect = require 'connect'
-sys = require 'sys'
-uuid = require 'uuid'
-io = require 'socket.io'
+Connect     = require 'connect'
+sys         = require 'sys'
+uuid        = require 'uuid'
+io          = require 'socket.io'
 
-couchdb = require 'couchdb'
-couchClient = couchdb.createClient $DB_PORT, $DB_SERVER
-db = couchClient.db 'chatbox'
+couchdb     = require 'couchdb'
+couchClient = couchdb.createClient DB_PORT, DB_SERVER
+db          = couchClient.db 'chatbox'
 
 # Connect middleware serves static files and can handle additional
 # features I might want to add later
 server = Connect.createServer(
+
         Connect.logger()
 
         Connect.staticProvider __dirname + '/public'
@@ -33,72 +34,60 @@ server = Connect.createServer(
                 res.writeHead 200, 'Content-Type': 'text/plain'
                 res.write 'Hello World'
                 res.end()
-        )
+)
 
-server.listen $PORT
-console.log "Listening on port " + $PORT + " with backend at " + $DB_SERVER + ":" + $DB_PORT
+server.listen PORT
+console.log "Listening on port #{PORT} with backend at #{DB_SERVER}: #{DB_PORT}"
 
-$ = JSON.stringify
+names = []
 
-users = (() ->
-        names = []
+users =
 
-        return {
-                getName: (id) ->
-                        return names[id]
+        getName: (id) ->
+                names[id]
 
-                setName: (id, newval) ->
-                        tmp = names[id]
-                        if not tmp
-                                names[id] = newval
-                                return "joined: " + names[id]
-                        else if tmp != newval
-                                names[id] = newval
-                                return tmp + " is now " + newval
-                        else return ''
+        setName: (id, newval) ->
+                oldval = names[id]
+                names[id] = newval
+                return "joined: #{names[id]}"
+                return "#{tmp} is now #{newval}" if oldval isnt newval
+                ''
 
-                destroy: (id) ->
-                        delete names[id]
+        destroy: (id) ->
+                delete names[id]
 
-                list: () ->
-                        output = ""
-                        for i in names
-                                if names.hasOwnProperty i
-                                        output += names[i] + ' '
-                        return output
-                }
-        )()
+        list: () ->
+                names.join ' '
 
+json = JSON.stringify
 
 # Socket.io hooks into the server above and intercepts socket comms
 socket = io.listen server
-socket.on 'connection', (client) ->
-        response = { name: "chatbot", content: "" }
 
-        client.send (() ->
-                response.content = "Welcome to chatbox! Other users online: " + users.list()
-                return $(response)
-                )()
+socket.on 'connection', (client) ->
+
+        response =
+                name: "chatbot",
+                content: "Welcome to chatbox! Other users online: #{users.list()}"
+
+        client.send json response
 
         client.on 'message', (message) ->
                 if message.system
-                        if message.system
-                                response.content = users.setName client.sessionId, message.name
-                                client.broadcast $(response)
+                        response.content = users.setName client.sessionId, message.name
+                        client.broadcast json response
                         return
-                #passing user message to Couch
-                db.saveDoc uuid.generate(), message, (er, ok) ->
-                        if er
-                                console.log 'DB error on input: ' +
-                                        $(message) +
-                                        $(er)
-                                throw new Error $(er)
+
+                #Passing user message to Couch
+                db.saveDoc uuid.generate(), message, (err, ok) ->
+                        if err
+                                console.log "DB error on input: #{sys.inspect(message)} #{sys.inspect(err)}"
+                                throw new Error err
                         else
-                                client.broadcast $(message)
-                                console.log 'Wrote to couch: ' +
-                                        sys.inspect message
+                                client.broadcast json message
+                                console.log "Wrote to couch: #{sys.inspect message}"
 
         client.on 'disconnect', () ->
                 response.content = users.getName(client.sessionId) + ' disconnected'
-                client.broadcast $(response)
+                client.broadcast json response
                 users.destroy client.sessionId
